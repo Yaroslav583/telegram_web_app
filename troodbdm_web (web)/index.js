66111a4fcf48308fe5c75f11211ab8b3c1127686
Yaroslav <makhinko180601@gmail.com>
2024-05-30 14:55:42 +0200

    fixed url

--- troodbdm_web (web)/index.js
+++ troodbdm_web (web)/index.js
@@ -5,7 +5,7 @@ const TELEGRAM_BOT_TOKEN = "7452926719:AAGB0dhxcmSWQLv-OQkHlpFAXT6nL8ZLy1E";
 const TELEGRAM_CHAT_ID = "411387721";
 
 exports.sendNotification = functions.https.onRequest((request, response) => {
-    const message = "Check out this page: https://your-web-app/page1";
+    const message = "Check out this page: https://troodbdm.web.app/page1";
     const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
 
     axios.post(url, {
