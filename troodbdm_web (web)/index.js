60303b20dc5770c5122d9d5113daa59e027aa076
Yaroslav <makhinko180601@gmail.com>
2024-05-31 13:20:42 +0200

    added vercel.json

--- troodbdm_web (web)/index.js
+++ troodbdm_web (web)/index.js
@@ -1,22 +1,16 @@
-const functions = require("firebase-functions");
-const axios = require("axios");
+const functions = require('firebase-functions');
+const TelegramBot = require('node-telegram-bot-api');
 
-const TELEGRAM_BOT_TOKEN = "7452926719:AAGB0dhxcmSWQLv-OQkHlpFAXT6nL8ZLy1E";
-const TELEGRAM_CHAT_ID = "411387721";
+// Токен вашего бота
+const token = '7452926719:AAGB0dhxcmSWQLv-OQkHlpFAXT6nL8ZLy1E';
+const bot = new TelegramBot(token, { polling: true });
 
-exports.sendNotification = functions.https.onRequest((request, response) => {
-    const message = "Check out this page: https://troodbdm.web.app/page1";
-    const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    const projectUrl = 'https://telegram-web-app-9e595.web.app/';
+    bot.sendMessage(chatId, `Check out: ${projectUrl}`);
+});
 
-    axios.post(url, {
-        chat_id: TELEGRAM_CHAT_ID,
-        text: message,
-    })
-        .then(() => {
-            response.send("Notification sent!");
-        })
-        .catch((error) => {
-            console.error("Error sending notification:", error);
-            response.status(500).send("Error sending notification");
-        });
+exports.bot = functions.https.onRequest((req, res) => {
+    bot.handleRequest(req, res);
 });
