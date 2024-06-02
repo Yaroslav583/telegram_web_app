d84119068e35fed8ad0ce256e8c173d5ee197ece
Yaroslav <makhinko180601@gmail.com>
2024-06-02 13:12:24 +0200

    fixed

---
+++ troodbdm_web/index.js
@@ -0,0 +1,17 @@
+const functions = require('firebase-functions');
+const TelegramBot = require('node-telegram-bot-api');
+
+
+const token = '7452926719:AAGB0dhxcmSWQLv-OQkHlpFAXT6nL8ZLy1E';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    const projectUrl = 't.me/yarik583_bot/fourdays';
+    bot.sendMessage(chatId, `Check: ${projectUrl}`);
+});
+
+
+exports.bot = functions.https.onRequest((req, res) => {
+    bot.handleRequest(req, res);
+});
